5fe5f287f967cf2184d8de31eaf2c2e28fa779d8
unknown <spremic.katarina@gmail.com>
2024-05-16 10:02:25 +0200

    Add EntityScan annotation

--- back/service/src/main/java/com/ftn/sbnz/service/ServiceApplication.java
+++ back/service/src/main/java/com/ftn/sbnz/service/ServiceApplication.java
@@ -4,16 +4,19 @@ import java.util.Arrays;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.kie.api.KieServices;
 import org.kie.api.builder.KieScanner;
 import org.kie.api.runtime.KieContainer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.ftn.sbnz")
+@EntityScan("com.ftn.sbnz.model.user")
 public class ServiceApplication  {
 	
 	private static Logger log = LoggerFactory.getLogger(ServiceApplication.class);
